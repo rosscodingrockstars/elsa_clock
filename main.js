de3d6427e12c8865b9b1c4e95eec6f5fb159948f
rosscodingrockstars <hey@jenniferross.tech>
2021-03-10 15:57:56 -0500

    fixed up broken things

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log($( "#singing-rehearsal" ).val())
+let timeSinging = $( "#singing-rehearsal" ).val();
+
+
+$("#singing-rehearsal").change(function() {
+ timeSinging = $( "#singing-rehearsal" ).val();
+
+  console.log(timeSinging)
+
+  });
+  // https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/doja-cat-1582767283.jpg
+
+  if (timeSinging==="10:00-10:30 am") {
+    
+  }

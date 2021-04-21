d399352d6cf2f9e34d249582ee932eff61d91ce8
rosscodingrockstars <hey@jenniferross.tech>
2021-04-21 16:59:21 -0400

    added major changes to button

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ $("#singing-rehearsal").change(function() {
         $("#default-img").attr("src", "https://theshaderoom.com/wp-content/uploads/2020/10/IMG_1081.jpg");
     
       }
+      
    
      });
 
@@ -38,10 +39,21 @@ $("#singing-rehearsal").change(function() {
      
        });
        $( "#partywoo" ).click(function() {
-        
-        $("#default-img").attr("src", "https://media.gettyimages.com/photos/group-of-friends-at-a-party-picture-id949040980?s=612x612");
+        let changeBack = true
 
+        
+        if (changeBack === true) {
+          $(this).css('background-color','#695d92');
+        $(this).text('uh oh i see moms car'); 
+        $("#default-img").attr("src", "https://media.gettyimages.com/photos/group-of-friends-at-a-party-picture-id949040980?s=612x612");
+        }
+       else if (changeBack === false) {
+          $(this).css('background-color','#828dca');
+        $(this).text('party woo'); 
+        $("#default-img").attr("src", "https://theshaderoom.com/wp-content/uploads/2020/10/IMG_1081.jpg");
+       }
       });
+    
   // https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/doja-cat-1582767283.jpg
   //https://static.billboard.com/files/2020/08/doja-cat-james-charles-makeup-tutorial-2020-video-still-billboard-1548-1597261378-compressed.jpg
   //https://townsquare.media/site/495/files/2020/02/Doja-Cat-2.jpg?w=980&q=75
